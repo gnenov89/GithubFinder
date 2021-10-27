e6a31fc1383be55d468b5c03182bdb3b2d56a094
Georgi <georgi.nenov89@gmail.com>
2021-10-27 10:58:46 -0400

    repos displaying

--- github.js
+++ github.js
@@ -1,16 +1,26 @@
 class GitHub {
     constructor(){
         this.client_id = '4372a65255ff0378f6a6';
-        this.client_secret = '6b7b431b8883cfa71cf0c093743145caec39fb1c'
+        this.client_secret = '6b7b431b8883cfa71cf0c093743145caec39fb1c';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
     }
 
     async getUser (user) {
+
         const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
       
+        const reposResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+         
+
         const profile = await profileResponse.json();
-        // repos 'https://api.github.com/users/gnenov89/repos'
+        
+        const repos = await reposResponse.json();
+
         return {
-            profile
+            profile,
+            repos
         }
     }
 }
